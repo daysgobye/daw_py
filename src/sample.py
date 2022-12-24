8492a57cbb05b60b54cce0dea13679e4df861a67
Cole Smith <smithcole@protonmail.com>
2022-12-23 18:02:37 -0800

    vlc

--- src/sample.py
+++ src/sample.py
@@ -1,6 +1,7 @@
 from collections.abc import Sequence
 from pytube import YouTube
 import os
+import  vlc
 
 
 class Source_Sample:
@@ -10,15 +11,30 @@ class Source_Sample:
         self.start=start
         self.end=end
         self.download()
+        self.play()
     def download(self):
         yt = YouTube(self.url)
         video = yt.streams.filter(only_audio=True).first()
         out_file = video.download(output_path="./samples")
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
-        os.rename(out_file, new_file)
+        try:
+            os.rename(out_file, new_file)
+        except FileExistsError:
+            print("error")
+            print(FileExistsError)
         self.path=new_file
     def play(self):
+        Instance = vlc.Instance()
+        player = Instance.media_player_new()
+        Media = Instance.media_new(self.path)
+        # Media.get_mrl()
+        Media.add_option(f'start-time={self.start}')
+        Media.add_option(f'stop-time={self.end}')
+        player.set_media(Media)
+        # player = vlc.MediaPlayer(self.path)
+        
+        player.play()
         pass
 
 
